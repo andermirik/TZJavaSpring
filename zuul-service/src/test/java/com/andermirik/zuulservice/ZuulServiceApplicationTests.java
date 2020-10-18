202d7004e13e9fc1f14b4fe928c014c50514dd55
andermirik <33791601+andermirik@users.noreply.github.com>
2020-10-18 18:31:44 +0300

    zuul, eureka

---
+++ zuul-service/src/test/java/com/andermirik/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andermirik.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
