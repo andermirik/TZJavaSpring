1a13d357047e53457a3a316e7ff7dbf4bc111d73
andermirik <33791601+andermirik@users.noreply.github.com>
2020-10-20 23:54:28 +0300

    zuulllllllllll

--- zuul-try-2/src/test/java/com/andermirik/zuultry2/ZuulTry2ApplicationTests.java
+++ zuul-try-2/src/test/java/com/andermirik/zuultry2/ZuulTry2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.andermirik.eurekaservice;
+package com.andermirik.zuultry2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-//class EurekaServiceApplicationTests {
+//class ZuulTry2ApplicationTests {
 //
 //	@Test
 //	void contextLoads() {
